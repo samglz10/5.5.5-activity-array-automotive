b621263b8b1ffc867e13fd063bbf39bdfbbd02a1
Samuel Gonzalez <samglz10@gmail.com>
2022-05-10 19:17:29 -0400

    final psuedocode

--- mecurySedan.js
+++ mecurySedan.js
@@ -16,4 +16,13 @@ class Car extends VehicleModule {
         super(make, model, year, color, mileage);
     }
 
+    super (makeCar);
 }
+
+/* I am assuming psuedo logic is in plain text, so here it goes
+
+loadPassenger(nums) if loadPassenger is less than maxinumPassengers then there is availableRoom
+
+start function if fule is greater than 0 then start equals true, else no start
+
+scheduleService (nums) if mileage is greater than 3000, time for maintenance is true == else no service
