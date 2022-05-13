7e25cb99a9174454dba34a7c23170c85e4bd753b
Samuel Gonzalez <samglz10@gmail.com>
2022-05-13 19:27:39 -0400

    upload new styles

--- mecurySedan.js
+++ mecurySedan.js
@@ -6,43 +6,42 @@ const VehicleModule = require("./vehicle.js")
 //console.log(v.make)
 
 class Car extends VehicleModule.Vehicle {
-    constructor (make, model, year, color, mileage) {
-        super(make, model, year, color, mileage);
-        this.maximnumPassengers = 5;
-        this.passenger = 0;
-        this.numberOfWheels = 4;
-        this.maxinumSpeed = 160;
-        this.fuel = 10;
-        this.scheduleService = false;
+	constructor (make, model, year, color, mileage) {
+		super(make, model, year, color, mileage);
+		this.maximnumPassengers = 5;
+		this.passenger = 0;
+		this.numberOfWheels = 4;
+		this.maxinumSpeed = 160;
+		this.fuel = 10;
+		this.scheduleService = false;
         
-    }
-    //loadPassenger(nums) if loadPassenger is less than maxinumPassengers then there is availableRoom
-loadPassenger (num) {
-    if (this.maximnumPassengers > this.passenger)
-        return (this.passenger + num);
-        else {
-        console.log("No Available Room")
-        }
-}
-//start function if fule is greater than 0 then start equals true, else no start
+	}
+	//loadPassenger(nums) if loadPassenger is less than maxinumPassengers then there is availableRoom
+	loadPassenger (num) {
+		if (this.maximnumPassengers > this.passenger)
+			return (this.passenger + num);
+		else {
+			console.log("No Available Room");
+		}
+	}
+	//start function if fule is greater than 0 then start equals true, else no start
 
-start () {
-    if (this.fuel > 0 ) {
-    console.log("Engine Start.") 
-    } else {
-        console.log("No Gaz")
-    }
-}
-//scheduleService (nums) if mileage is greater than 3000, time for maintenance is true == else no service
-scheduleService (num) {
-    if (this.scheduleService > 3000) {
-    console.log("Time for Service") 
-    } else {
-        console.log()
-    }
-}
+	start () {
+		if (this.fuel > 0 ) {
+			console.log("Engine Start.");
+		} else {
+			console.log("No Gaz");
+		}
+	}
+	//scheduleService (nums) if mileage is greater than 3000, time for maintenance is true == else no service
+	scheduleService (num) {
+		if (this.scheduleService > 3000) {
+			console.log("Time for Service"); 
+		} else {
+			console.log();
+		}
+	}
 }
 
 //console.log(v.make);
 // I am assuming psuedo logic is in plain text, so here it goes
-
